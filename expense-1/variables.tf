21569a883f531303e2da0ab950070d48c7e8c0ca
ramak8sgcp <ramak8sgcp@gmail.com>
2025-02-02 19:38:02 +0530

    terraform

---
+++ expense-1/variables.tf
@@ -0,0 +1,14 @@
+variable "instance_names" {
+  type    = list(string)
+  default = ["mysql", "backend", "frontend"]
+  description = "description"
+}
+
+
+variable "domain_name" {
+  default = "ramana3490.online"
+}
+
+variable "zone_id" {
+  default = "Z07026951IZYV04HDC3NF"
+}
