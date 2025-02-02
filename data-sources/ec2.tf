3b88634bf17631b995d5a89a3592d0b212ed1156
ramak8sgcp <ramak8sgcp@gmail.com>
2025-02-02 18:45:38 +0530

    terraform

--- data-sources/ec2.tf
+++ data-sources/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform" {
   
-  ami                    = "data.aws_ami.joindevops.id"
+  ami                    = data.aws_ami.joindevops.id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
   tags = {
