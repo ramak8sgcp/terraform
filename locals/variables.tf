50ede652de260a728c092fbc36e7c0d5d3c4babb
ramak8sgcp <ramak8sgcp@gmail.com>
2025-02-02 22:27:11 +0530

    terraform

---
+++ locals/variables.tf
@@ -0,0 +1,18 @@
+variable "instance_names" {
+  type    = list(string)
+  default = ["mysql", "backend", "frontend"]
+  description = "description"
+}
+
+
+# variable "domain_name" {
+#   default = "ramana3490.online"
+# }
+
+# variable "zone_id" {
+#   default = "Z07026951IZYV04HDC3NF"
+# }
+
+variable "environment"{
+  default = "prod"
+}
