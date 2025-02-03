a6d54134445ef9ce69ae822efb30240128216ff6
ramak8sgcp <ramak8sgcp@gmail.com>
2025-02-03 10:59:31 +0530

    terraform

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.micro"
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
 
   tags = {
@@ -12,15 +12,15 @@ resource "aws_instance" "terraform" {
   }
 
   connection {
-    type = "ssh"
-    user = "ec2-user"
+    type     = "ssh"
+    user     = "ec2-user"
     password = "DevOps321"
-    host = self.public_ip
+    host     = self.public_ip
   }
 
   # provisioners will execute at the time of creation
-  provisioner = "remote-exec" {
-    inline  = [
+  provisioner "remote-exec" {
+    inline = [
       "sudo dnf install ansible -y",
       "sudo dnf install nginx -y",
       "sudo systemctl start nginx",
@@ -29,42 +29,42 @@ resource "aws_instance" "terraform" {
 
   provisioner "remote-exec" {
     when = destroy
-    inline = [ 
+    inline = [
       "sudo systemctl stop nginx",
     ]
   }
 }
 
 resource "aws_security_group" "allow_ssh_terraform" {
-    name        =  "allow-ssh"  #allow_ssh is already ther in my account
-    description = "Allo port number 22 for SSH access"
+  name        = "allow-ssh" #allow_ssh is already ther in my account
+  description = "Allo port number 22 for SSH access"
 
-    #usually we allow everything in egress
-    egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
+  #usually we allow everything in egress
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-    ingress {
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]    #allow from everyone
-      ipv6_cidr_blocks = ["::/0"]
-    }
-    
-    ingress {
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]    #allow from everyone
-      ipv6_cidr_blocks = ["::/0"]
-    }
-    
-    tags  = {
-      Name = "allow_sshh"
-    }
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_sshh"
+  }
 }
