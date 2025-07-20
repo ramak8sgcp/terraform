3d5e51870b35ff3ed4ed8f317b0e14700a1adff6
ramak8sgcp <ramak8sgcp@gmail.com>
2025-07-20 22:22:33 +0530

    updating

--- data-sources/ec2.tf
+++ data-sources/ec2.tf
@@ -23,8 +23,8 @@ resource "aws_security_group" "allow_ssh_terraform" {
   }
 
   ingress {
-    from_port        = 0
-    to_port          = 0
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
     ipv6_cidr_blocks = ["::/0"]
